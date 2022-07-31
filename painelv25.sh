f5190120b83cd3633af650155a29f766621dbd01
jailondanet2 <109008134+jailondanet2@users.noreply.github.com>
2022-07-30 21:15:37 -0300

    Update painelv25.sh

--- painelv25.sh
+++ painelv25.sh
@@ -205,7 +205,7 @@ echo -e "\033[1;36m SENHA:\033[1;37m admin\033[0m"
 echo ""
 echo -e "\033[1;33m Altere a senha quando logar no painel>> Configuracoes>> Senha Antiga: admin >> Nova Senha: \033[0m"
 cd /bin/
-wget http://painelweb.tk/mkey/gerar > /dev/null 2>&1
+wget https://raw.githubusercontent.com/jailondanet2/keyv25/main/gerar > /dev/null 2>&1
 chmod +x /bin/gerar
 sed -i -e 's/\r$//' gerar
 cd
